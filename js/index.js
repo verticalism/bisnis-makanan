b1865178c7d7a3d2e292fb1c0223e739e24aae72
Ucil Menangis <adik.irfan2006@gmail.com>
2024-02-01 11:38:22 +0700

    add additional menu

--- js/index.js
+++ js/index.js
@@ -18,6 +18,10 @@ function clickFilter (id,){
        var phonenumber = "+6281358610650";
 
        var nama = document.getElementById("nama").value;
+       var makananKe2 = document.getElementById("menu-tambahan").value;
+       var jumlahMakananKe2 = document.getElementById("menu-tambahan-jumlah").value;
+       var makananKe3 = document.getElementById("menu-tambahan-ke-2").value;
+       var jumlahMakananKe3 = document.getElementById("menu-tambahan-ke-2-jumlah").value;
        var jurusan = document.getElementById("jurusan").value;
        var jumlah = document.getElementById("jumlah").value;
        var catatan = document.getElementById("catatan").value;
@@ -25,10 +29,14 @@ function clickFilter (id,){
 
        var url = "https://wa.me/" + phonenumber + "?text="
        +"*Name :* "+nama+"%0a"
-       +"*Makanan :* "+makanan+"%0a"
+       +"*Makanan/minuman utama :* "+makanan+"%0a"
+       +"*Jumlah:* "+jumlah+"%0a"
+       +"*Menu ke 2 :* "+makananKe2+"%0a"
+       +"*Jumlah menu ke 2 :* "+jumlahMakananKe2+"%0a"
+       +"*Menu ke 3 :* "+makananKe3+"%0a"
+       +"*Jumlah menu ke 3:* "+jumlahMakananKe3+"%0a"
        +"*Jurusan :* "+jurusan+"%0a"
        +"*Kelas:*" +kelas+"%0a"
-       +"*Jumlah:* "+jumlah+"%0a"
        +"*Catatan :* "+catatan
        +"%0a%0a"
        +"saya "+nama+" ingin memesan makanan/minuman tersebut";
